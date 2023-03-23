3f73c03c97986cb9b61c5cac4223c27628e2e015
aksu11 <aki.sukkela@hotmail.com>
2023-03-23 15:02:41 +0200

    first commit

---
+++ anekdootit/src/App.js
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+
+const anecdotes = [
+	'If it hurts, do it more often.',
+	'Adding manpower to a late software project makes it later!',
+	'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+	'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+	'Premature optimization is the root of all evil.',
+	'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+	'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+	'The only way to go fast, is to go well.'
+]
+ 
+
+const App = () => {
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(Array(anecdotes.length).fill(0))
+	const [mostVoted, setMostvoted] = useState(null)
+	console.log('selected: ', selected)
+
+  const vote = () => {
+    const copy = [ ...points ]
+    copy[selected] = copy[selected] + 1  
+    setMostvoted(copy.indexOf(Math.max(...copy)))
+    setPoints(copy)
+  }
+  
+  const draw = () => {
+    console.log(points)
+    console.log('mostvoted: ', mostVoted)
+		let rand = selected
+    do {
+      rand = Math.floor(Math.random() * anecdotes.length)
+    } while (rand === selected)
+    setSelected(rand)
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <div>{anecdotes[selected]}</div>
+      <div>has {points[selected]} votes</div>
+      <button onClick={() => vote()}>Vote</button>
+      <button onClick={() => draw()}>Next anecdote</button>
+			<div>
+        <h1>Anecdote with most votes</h1>
+        <div>{anecdotes[mostVoted]}</div>
+        {mostVoted !== null ? <div>has {points[mostVoted]} votes</div> : null}
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ unicafe/src/App.js
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+
+const StatisticLine = (props) => <tr><td>{props.text}</td><td>{props.value}</td></tr>
+
+const Statistics = (props) => {
+  const good = props.values.good
+  const neutral = props.values.neutral
+  const bad = props.values.bad
+  const all = props.values.good + props.values.neutral + props.values.bad
+  const average = ((props.values.good * 1 + props.values.bad * -1) / all).toFixed(1)
+  const positive = (good / all * 100).toFixed(1).toString() + " %"
+
+  if (all === 0) return (
+    <div>No feedback given</div>
+  )
+
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="Good" value={good} />
+        <StatisticLine text="Neutral" value={neutral} />
+        <StatisticLine text="Bad" value={bad} />
+        <StatisticLine text="All" value={all} />
+        <StatisticLine text="Average" value={average} />
+        <StatisticLine text="Positive" value={positive} />
+      </tbody>
+    </table>
+  )
+
+}
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+
+const App = () => {
+
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="Good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="Bad" />
+      <h1>Statistics</h1>
+      <Statistics values={{good: good, neutral: neutral, bad: bad}} />
+    </div>
+  )
+}
+
+export default App
